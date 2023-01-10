01784a6ab17c34efbe3eca0a977e05b675eb3e26
Masud Rana <masud.pust16@gmail.com>
2023-01-10 22:18:57 +0600

    we split

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Md. Masud Rana on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+            HomeScreen()
+        }
+    }
+}
